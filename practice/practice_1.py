85e3e2a290be82c77b27ac11f7c83f124cf8113e
snow-oc <snow7349.oc@gmail.com>
2025-08-13 00:31:12 +0900

    add_task関数追加

---
+++ practice/practice_1.py
@@ -0,0 +1,14 @@
+# --- 課題① ---
+# 空のリスト todo_list を作って、そこにタスク（文字列）を追加する関数 add_task(task) を作ろう。
+# 追加後に現在のタスク数をprintで表示しよう。
+# 動作確認として、関数を使って3つくらいタスクを追加してみてね。
+
+todo_list = []
+
+def add_task(task):
+    todo_list.append(task)
+    print(f"現在のタスク数は、{len(todo_list)}です")
+
+add_task("海に行く")
+add_task("勉強をする")
+add_task("本を読む")
