db323f8ddaec92c387bc28348324d4e20def75c5
snow-oc <snow7349.oc@gmail.com>
2025-08-14 00:31:39 +0900

    課題1修正

--- practice/practice_1.py
+++ practice/practice_1.py
@@ -12,15 +12,3 @@ def add_task(task):
 add_task("海に行く")
 add_task("勉強をする")
 add_task("本を読む")
-
-# --- 課題② ---
-# todo_list の中身をすべて表示する関数 show_tasks() を作ろう
-# タスクが何もなければ「タスクはありません」と表示するようにしてね
-def show_tasks():
-    if len(todo_list) == 0:
-        print("タスクはありません")
-        return
-    for task in todo_list:
-        print(task)
-
-show_tasks()
